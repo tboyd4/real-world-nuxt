e07170daa8374e9438e41d59a485c265249afd82
tboyd <tboyd1898@gmail.com>
2020-03-11 15:58:43 -0500

    axios calling fetching event lists from db.json

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,42 @@
 <template>
   <div>
     <h1>Events</h1>
+    <event-card
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    ></event-card>
   </div>
 </template>
 
 <script>
-  export default {
-    head() {
-      return {
-        title: 'Event Listing'
-      }
+import EventCard from '@/components/EventCard.vue'
+export default {
+  head() {
+    return {
+      title: 'Event Listing'
     }
+  },
+  components: {
+    EventCard
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3001/events')
+      .then(response => {
+        return {
+          events: response.data
+        }
+      })
+      .catch(e => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time.'
+        })
+      })
   }
+}
 </script>
 
-<style  scoped>
-</style>
+<style scoped></style>
